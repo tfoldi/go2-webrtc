904584c1643b17a4ed5aaed73d0ef09b84937893
Földi Tamás <tfoldi@xsi.hu>
2024-02-14 16:34:04 +0100

    feat: new object API instead of functional

--- index.js
+++ index.js
@@ -77,105 +77,90 @@ function logMessage(text) {
   msg.textContent += truncateString(text, 300) + "\n";
   log.scrollTop = log.scrollHeight;
 }
+class WebRTCClient {
+  constructor(token, robotIP) {
+    this.token = token;
+    this.robotIP = robotIP;
+    this.msgCallbacks = new Map();
+    this.validationResult = "PENDING";
+    this.pc = new RTCPeerConnection({ sdpSemantics: "unified-plan" });
+    this.channel = this.pc.createDataChannel("data");
 
-// main function to initialize the RTC connection
-// called when the connect button is clicked
-function initRTC() {
-  var ret = {};
+    this.pc.addTransceiver("video", { direction: "recvonly" });
+    this.pc.addTransceiver("audio", { direction: "sendrecv" });
+    this.pc.addEventListener("track", this.trackEventHandler.bind(this));
+    this.channel.onmessage = this.messageEventHandler.bind(this);
 
-  ret.msgCallbacks = new Map();
-  ret.validationResult = "PENDING";
+    this.heartbeatTimer = null;
+  }
 
-  ret.pc = new RTCPeerConnection({ sdpSemantics: "unified-plan" });
-  console.log("Created RTCPeerConnection");
-  logMessage("Created RTCPeerConnection");
+  trackEventHandler(event) {
+    if (event.track.kind === "video") {
+      this.VidTrackEvent = event;
+    } else {
+      this.AudTrackEvent = event;
+    }
+  }
 
-  ret.pc.addTransceiver("video", { direction: "recvonly" }),
-    ret.pc.addTransceiver("audio", { direction: "sendrecv" }),
-    ret.pc.addEventListener("track", (event) => {
-      event.track.kind === "video"
-        ? (ret.VidTrackEvent = event)
-        : (ret.AudTrackEvent = event);
-    });
-
-  ret.channel = ret.pc.createDataChannel("data");
-
-  ret.channel.onmessage = (event) => {
+  messageEventHandler(event) {
     if (event.data && !event.data.includes("heartbeat")) {
       console.log("onmessage", event);
-      handleDataChannelMessage(rtc, event);
+      this.handleDataChannelMessage(event);
     }
-  };
-
-  initSDP(ret);
-  return ret;
-}
-
-function handleDataChannelMessage(rtc, event) {
-  //   console.log("Data channel message received:", event.data);
-
-  const data =
-    typeof event.data == "string"
-      ? JSON.parse(event.data)
-      : dealArrayBuffer(event.data);
-
-  if (data.type === DataChannelType.VALIDATION) {
-    rtcValidation(rtc, data);
   }
-  logMessage(`-> msg type:${event.type} data:${event.data}`);
-}
 
-function dealArrayBuffer(n) {
-  const o = new Uint16Array(n.slice(0, 2)),
-    s = n.slice(4, 4 + o[0]),
-    c = n.slice(4 + o[0]),
-    u = new TextDecoder("utf-8"),
-    l = JSON.parse(u.decode(s));
-  return (l.data.data = c), l;
-}
+  handleDataChannelMessage(event) {
+    const data =
+      typeof event.data == "string"
+        ? JSON.parse(event.data)
+        : this.dealArrayBuffer(event.data);
+    if (data.type === DataChannelType.VALIDATION) {
+      this.rtcValidation(data);
+    }
+    logMessage(`-> msg type:${event.type} data:${event.data}`);
+  }
 
-// Function to format date according to unitree's requirements
-function formatDate(r) {
-  const n = r,
-    y = n.getFullYear(),
-    m = ("0" + (n.getMonth() + 1)).slice(-2),
-    d = ("0" + n.getDate()).slice(-2),
-    hh = ("0" + n.getHours()).slice(-2),
-    mm = ("0" + n.getMinutes()).slice(-2),
-    ss = ("0" + n.getSeconds()).slice(-2);
-  return y + "-" + m + "-" + d + " " + hh + ":" + mm + ":" + ss;
-}
+  dealArrayBuffer(n) {
+    const o = new Uint16Array(n.slice(0, 2)),
+      s = n.slice(4, 4 + o[0]),
+      c = n.slice(4 + o[0]),
+      u = new TextDecoder("utf-8"),
+      l = JSON.parse(u.decode(s));
+    return (l.data.data = c), l;
+  }
 
-function initSDP(rtc) {
-  rtc.pc
-    .createOffer()
-    .then((offer) => rtc.pc.setLocalDescription(offer))
-    .then(() => {
-      console.log("Offer created");
-      logMessage("Offer created");
-      console.log(rtc.pc.localDescription);
-      logMessage(rtc.pc.localDescription);
-      initSignaling(rtc);
-    })
-    .catch(console.error);
-}
+  initSDP() {
+    this.pc
+      .createOffer()
+      .then((offer) => this.pc.setLocalDescription(offer))
+      .then(() => {
+        console.log("Offer created");
+        logMessage("Offer created");
+        console.log(this.pc.localDescription);
+        logMessage(this.pc.localDescription);
+        this.initSignaling();
+      })
+      .catch(console.error);
+  }
 
-// Function to send the offer to the signaling server
-// This is where you would send the offer to the robot
-function initSignaling(rtc) {
-  function sendMessage(message) {
+  initSignaling() {
+    var answer = {
+      token: this.token,
+      id: "STA_localNetwork",
+      type: "offer",
+    };
+    answer["sdp"] = this.pc.localDescription.sdp;
+    console.log(answer);
+
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(message),
+      body: JSON.stringify(answer),
     };
 
-    fetch(
-      `http://${document.getElementById("robot-ip").value}:8081/offer`,
-      options
-    )
+    fetch(`http://${this.robotIP}:8081/offer`, options)
       .then((response) => {
         console.log(`statusCode: ${response.status}`);
         return response.json();
@@ -183,11 +168,11 @@ function initSignaling(rtc) {
       .then((data) => {
         console.log("Response from signaling server:" + JSON.stringify(data));
         logMessage("Establishing connection...");
-        rtc.pc
+        this.pc
           .setRemoteDescription(data)
           .then(() => {
             logMessage("WebRTC connection established");
-            startHeartbeat(rtc);
+            this.startHeartbeat();
           })
           .catch((e) => {
             console.log(e);
@@ -197,124 +182,123 @@ function initSignaling(rtc) {
         console.error("Error sending message:", error);
       });
   }
-  var answer = {
-    token: document.getElementById("token").value,
-    id: "STA_localNetwork",
-    type: "offer",
-  };
-  answer["sdp"] = rtc.pc.localDescription.sdp;
-  console.log(answer);
-  sendMessage(answer);
-}
 
-function startHeartbeat(rtc) {
-  this.heartbeatTimer = window.setInterval(() => {
-    const date = new Date();
-    (rtc.channel == null ? void 0 : rtc.channel.readyState) === "open" &&
-      (rtc.channel == null ||
-        rtc.channel.send(
-          JSON.stringify({
-            type: DataChannelType.HEARTBEAT,
-            data: {
-              timeInStr: formatDate(date),
-              timeInNum: Math.floor(date.valueOf() / 1e3),
-            },
-          })
-        ));
-  }, 2e3);
-}
-
-function rtcValidation(rtc, msg) {
-  if( msg.data === "Validation Ok.") {
-    logMessage("Validation OK");
-    rtc.validationResult = "SUCCESS";
-  } else {
-    logMessage(`Sending validation key ${msg.data}`);
-    publish(rtc, "", encryptKey(msg.data), DataChannelType.VALIDATION); // );
-
-    // TODO: execute all the registred callbacks in a map defined
-    // in the initRTC function
-    logMessage("Sending video on message");
-    publish(rtc, "", "on", DataChannelType.VID);
-    // TODO this should be on the callback for video on message
-    logMessage("Playing video");
-    document.getElementById("video-frame").srcObject = rtc.VidTrackEvent.streams[0]
+  startHeartbeat() {
+    this.heartbeatTimer = window.setInterval(() => {
+      const date = new Date();
+      (this.channel == null ? void 0 : this.channel.readyState) === "open" &&
+        (this.channel == null ||
+          this.channel.send(
+            JSON.stringify({
+              type: DataChannelType.HEARTBEAT,
+              data: {
+                timeInStr: this.formatDate(date),
+                timeInNum: Math.floor(date.valueOf() / 1e3),
+              },
+            })
+          ));
+    }, 2e3);
   }
-}
 
-globalThis.rtcValidation = rtcValidation;
+  rtcValidation(msg) {
+    if (msg.data === "Validation Ok.") {
+      logMessage("Validation OK");
+      this.validationResult = "SUCCESS";
 
-// TODO: what is the purpose of this function?
-function dealMsgKey(n, o, s) {
-  return s || `${n} $ ${o}`;
-}
+      // TODO: execute all the registred callbacks in a map defined
+      // in the initRTC function
+      logMessage("Sending video on message");
+      this.publish("", "on", DataChannelType.VID);
+      // TODO this should be on the callback for video on message
+      logMessage("Playing video");
+      document.getElementById("video-frame").srcObject =
+        this.VidTrackEvent.streams[0];
+    } else {
+      logMessage(`Sending validation key ${msg.data}`);
+      this.publish("", encryptKey(msg.data), DataChannelType.VALIDATION); // );
+    }
+  }
+  // Function to format date according to unitree's requirements
+  formatDate(r) {
+    const n = r,
+      y = n.getFullYear(),
+      m = ("0" + (n.getMonth() + 1)).slice(-2),
+      d = ("0" + n.getDate()).slice(-2),
+      hh = ("0" + n.getHours()).slice(-2),
+      mm = ("0" + n.getMinutes()).slice(-2),
+      ss = ("0" + n.getSeconds()).slice(-2);
+    return y + "-" + m + "-" + d + " " + hh + ":" + mm + ":" + ss;
+  }
 
-function saveResolve(rtc, channelType, channel, res, id) {
-  const msgKey = dealMsgKey(channelType, channel, id),
-    callback = rtc.msgCallbacks.get(msgKey);
-  callback ? callback.push(res) : rtc.msgCallbacks.set(msgKey, [res]);
-}
+  dealMsgKey(channelType, channel, id) {
+    return id || `${channelType} $ ${channel}`;
+  }
 
-function publish(rtc, topic, data, channelType) {
-  logMessage(`<- msg type:${channelType} topic:${topic} data:${JSON.stringify(data)}`);
-  return new Promise((resolve, reject) => {
-    var ch, f, _;
-    console.log("publish: ", topic, data, rtc.channel, channelType);
-    ((ch = rtc.channel) == null ? void 0 : ch.readyState) === "open"
-      ? (rtc.channel.send(
-          JSON.stringify({
-            type: channelType || DataChannelType.MSG,
-            topic: topic,
-            data: data,
-          })
-        ),
-        saveResolve(
-          rtc,
+  saveResolve(channelType, channel, res, id) {
+    const msgKey = this.dealMsgKey(channelType, channel, id),
+      callback = this.msgCallbacks.get(msgKey);
+    callback ? callback.push(res) : this.msgCallbacks.set(msgKey, [res]);
+  }
+
+  publish(topic, data, channelType) {
+    logMessage(
+      `<- msg type:${channelType} topic:${topic} data:${JSON.stringify(data)}`
+    );
+    return new Promise((resolve, reject) => {
+      if (this.channel && this.channel.readyState === "open") {
+        const msg = {
+          type: channelType || DataChannelType.MSG,
+          topic: topic,
+          data: data,
+        };
+        this.channel.send(JSON.stringify(msg));
+        const id =
+          data && data.uuid
+            ? data.uuid
+            : data && data.header && data.header.identity.id;
+        this.saveResolve(
           channelType || DataChannelType.MSG,
           topic,
           resolve,
-          data.uuid ||
-            ((_ =
-              (f = data == null ? void 0 : data.header) == null
-                ? void 0
-                : f.identity) == null
-              ? void 0
-              : _.id)
-        ))
-      : (console.error("data channel is not open", topic),
-        reject("data channel is not open"));
-  });
-}
+          id
+        );
+      } else {
+        console.error("data channel is not open", topic);
+        reject("data channel is not open");
+      }
+    });
+  }
 
-// Function to publish a message to the robot with full header
-//  .publishReqNew(topic, { //     api_id: s.api_id,
-//     data: s.data,
-//     id: s.id,
-//     priority: !!s.priority,
-//   })
-function publishReqNew(rtc, topic, msg) {
-  const uniqID =
-    (new Date().valueOf() % 2147483648) + Math.floor(Math.random() * 1e3);
-  if (!(msg != null && msg.api_id))
-    return console.error("missing api id"), Promise.reject("missing api id");
-  const _msg = {
-    header: {
-      identity: {
-        id: msg.id || uniqID,
-        api_id: (msg == null ? void 0 : msg.api_id) || 0,
+  // Function to publish a message to the robot with full header
+  //  .publishReqNew(topic, { //     api_id: s.api_id,
+  //     data: s.data,
+  //     id: s.id,
+  //     priority: !!s.priority,
+  //   })
+  publishReqNew(topic, msg) {
+    const uniqID =
+      (new Date().valueOf() % 2147483648) + Math.floor(Math.random() * 1e3);
+    if (!(msg != null && msg.api_id))
+      return console.error("missing api id"), Promise.reject("missing api id");
+    const _msg = {
+      header: {
+        identity: {
+          id: msg.id || uniqID,
+          api_id: (msg == null ? void 0 : msg.api_id) || 0,
+        },
       },
-    },
-    parameter: "",
-  };
-  return (
-    msg != null &&
-      msg.data &&
-      (_msg.parameter =
-        typeof msg.data == "string" ? msg.data : JSON.stringify(msg.data)),
-    msg != null && msg.priority && (_msg.header.policy = { priority: 1 }),
-    publish(rtc, topic, _msg, DataChannelType.REQUEST)
-    // publish(rtc, topic,  {api_id: 1016, data: 1016}, DataChannelType.REQUEST)
-  );
+      parameter: "",
+    };
+    return (
+      msg != null &&
+        msg.data &&
+        (_msg.parameter =
+          typeof msg.data == "string" ? msg.data : JSON.stringify(msg.data)),
+      msg != null && msg.priority && (_msg.header.policy = { priority: 1 }),
+      this.publish(topic, _msg, DataChannelType.REQUEST)
+      // publish(rtc, topic,  {api_id: 1016, data: 1016}, DataChannelType.REQUEST)
+    );
+  }
 }
 
 // Function to load saved values from localStorage
@@ -354,7 +338,8 @@ function handleConnectClick() {
   saveValuesToLocalStorage();
 
   // Initialize RTC
-  rtc = initRTC();
+  rtc = new WebRTCClient(token, robotIP);
+  rtc.initSDP();
   globalThis.rtc = rtc;
 
   // Send the offer to the signaling server
@@ -362,9 +347,18 @@ function handleConnectClick() {
 }
 
 function handleExecuteClick() {
-    const command = document.getElementById("command").value;
-    console.log("Command:", command);
-    publishReqNew(rtc, "rt/api/sport/request", {api_id: 1016, data: 1016})
+  const uniqID =
+    (new Date().valueOf() % 2147483648) + Math.floor(Math.random() * 1e3);
+  const command = parseInt(document.getElementById("command").value);
+
+  console.log("Command:", command);
+
+    globalThis.rtc.publish("rt/api/sport/request", {
+      header: { identity: { id: uniqID, api_id: command } },
+      parameter: JSON.stringify(command),
+      api_id: command,
+    });
+
 }
 
 function truncateString(str, maxLength) {
@@ -379,7 +373,6 @@ function truncateString(str, maxLength) {
   }
 }
 
-
 // Load saved values when the page loads
 document.addEventListener("DOMContentLoaded", loadSavedValues);
 
@@ -388,7 +381,7 @@ document
   .getElementById("connect-btn")
   .addEventListener("click", handleConnectClick);
 
-  document
+document
   .getElementById("execute-btn")
   .addEventListener("click", handleExecuteClick);
 

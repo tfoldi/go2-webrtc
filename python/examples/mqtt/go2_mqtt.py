d6c0db7118006a08303eb706eda5f2bdf8d9781a
Földi Tamás <tfoldi@xsi.hu>
2024-02-18 19:37:49 +0100

    feat: add bidirectional support for MQTT

--- python/examples/mqtt/go2_mqtt.py
+++ python/examples/mqtt/go2_mqtt.py
@@ -26,9 +26,12 @@ import os
 import paho.mqtt.client as mqtt
 import logging
 import json
+from queue import Queue, Empty
+
 
 from go2_webrtc import Go2Connection, RTC_TOPIC
 
+MQTT_TOPIC = "/rt/mqtt/request"
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -39,6 +42,21 @@ class Go2MQTTBridge:
     def __init__(self):
         self.mqtt_client = None
         self.initialize_mqtt_client()
+        self.msg_queue = Queue()
+        self.conn = None
+
+    def on_connect(self, client, userdata, flags, rc, properties):
+        logger.debug("MQTT: Connected with result code %s", rc)
+        result, mid = self.mqtt_client.subscribe(MQTT_TOPIC)
+        if result == mqtt.MQTT_ERR_SUCCESS:
+            logger.debug(f"MQTT: subscription to {MQTT_TOPIC} succeeded with MID {mid}")
+        else:
+            logger.debug(
+                f"MQTT: subscription to {MQTT_TOPIC} failed with result code {result}"
+            )
+
+    def on_message(self, client, userdata, msg):
+        self.msg_queue.put(msg.payload)
 
     def initialize_mqtt_client(self):
         mqtt_broker = os.getenv("MQTT_BROKER")
@@ -49,25 +67,47 @@ class Go2MQTTBridge:
         self.mqtt_client = mqtt.Client(
             mqtt.CallbackAPIVersion.VERSION2, client_id="Go2", protocol=mqtt.MQTTv311
         )
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+        self.mqtt_client.enable_logger()
 
         self.mqtt_client.username_pw_set(username=mqtt_username, password=mqtt_password)
         self.mqtt_client.connect(host=mqtt_broker, port=mqtt_port, keepalive=120)
         self.mqtt_client.loop_start()
 
     async def mqtt_loop(self, conn):
+        self.conn = conn
+
         while True:
-            await asyncio.sleep(1)
+            if self.conn.data_channel.readyState == "open":
+                while True:
+                    try:
+                        while not self.msg_queue.empty():
+                            msg = self.msg_queue.get_nowait()
+                            msg = msg.decode("utf-8")
+                            logger.debug(f"MQTT->RTC Sending message {msg} to Go2")
+                            conn.data_channel.send(msg)
+                    except Empty:
+                        pass
+                    # wait for a short time before checking the queue again
+                    await asyncio.sleep(0.1)
+            else:
+                # wait until the data channel is open
+                await asyncio.sleep(1)
 
     def on_validated(self):
         for topic in RTC_TOPIC.values():
             conn.data_channel.send(json.dumps({"type": "subscribe", "topic": topic}))
 
     def on_data_channel_message(self, message, msgobj):
-        logger.debug(msgobj)
-        logger.debug("GO2->MQTT Received message type=%s topic=%s", msgobj.get("type"),  msgobj.get("topic"))
+        logger.debug(
+            "RTC->MQTT Received message type=%s topic=%s",
+            msgobj.get("type"),
+            msgobj.get("topic"),
+        )
 
         if self.mqtt_client:
-            self.mqtt_client.publish(msgobj.get("topic","rt/system"), message, qos=1)
+            self.mqtt_client.publish(msgobj.get("topic", "rt/system"), message, qos=1)
         else:
             logger.warn("MQTT client not initialized")
 
@@ -84,9 +124,6 @@ if __name__ == "__main__":
         on_validated=mqtt_bridge.on_validated,
         on_message=mqtt_bridge.on_data_channel_message,
     )
-
-    # connect to Go2
-    # coro = mqtt_bridge.mqtt_loop(conn)
 
     loop = asyncio.get_event_loop()
 
